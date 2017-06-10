c59d8608ea92ba8760fda809ac518db32aa56303
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-06-09 22:10:17 -0400

    This is a new commit for what I originally planned to be amended

---
+++ app.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+  var quote;
+  var author;
+
+function getQuote() {
+$.ajax({
+  url: 'https://api.forismatic.com/api/1.0/',
+  jsonp: 'jsonp',
+  dataType: 'jsonp',
+  data: {
+    method: 'getQuote',
+    lang: 'en',
+    format: 'jsonp'
+  },
+
+success: function(response) {
+  quote = response.quoteText;
+  author = response.quoteAuthor;
+  $('#quote').text(quote);
+  if (author) {
+    $('#author').text('said by ' + author);
+  } else {
+    $('#author').text('- unknown');
+  }
+}
+});
+
+}
+
+getQuote();
+
+$('#getQuote').on('click', function(event) {
+  getQuote();
+event.preventDefault();
+})
+$('#tweet').on('click', function(event){
+  event.preventDefault();
+  window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote + '--' + author));
+})
+  });
